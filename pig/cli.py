779bcf6639023575cb4fe0b010e452d00be4c283
Fco Junior <fcojunr@gmail.com>
2018-06-29 00:01:14 -0300

    short description

--- pig/cli.py
+++ pig/cli.py
@@ -4,5 +4,6 @@ import click
 @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
 @click.argument('name', default='world', required=False)
 def main(name, as_cowboy):
+  """{{ project_short_description }}"""
   greet = 'Howdy' if as_cowboy else 'Hello'
   click.echo('{0}, {1}.'.format(greet, name))
