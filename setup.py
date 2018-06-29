754ab859170d470c8868b57717e1ac46e56de9f7
Fco Junior <fcojunr@gmail.com>
2018-06-28 23:58:36 -0300

    project configuration

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name='pig-py',
+  version='0.0.1',
+  author="Francisco Junior",
+  author_email="fcojunr@gmail.com",
+  description="A package for generate icons for Ionic",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/FcoJunior/pig-py",
+  packages=find_packages(),
+  include_package_data=True,
+  zip_safe=False,
+  platforms='any',
+  install_requires=dependencies,
+  entry_points={
+    'console_scripts': [
+      'pig = pig.cli:main',
+    ],
+  },
+  classifiers=[
+    # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    'Development Status :: 1 - Planning',
+    # 'Development Status :: 2 - Pre-Alpha',
+    # 'Development Status :: 3 - Alpha',
+    # 'Development Status :: 4 - Beta',
+    # 'Development Status :: 5 - Production/Stable',
+    # 'Development Status :: 6 - Mature',
+    # 'Development Status :: 7 - Inactive',
+    # 'Environment :: Console',
+    # 'Intended Audience :: Developers',
+    # 'License :: OSI Approved :: BSD License',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS',
+    'Operating System :: Unix',
+    'Operating System :: Microsoft :: Windows',
+    # 'Programming Language :: Python',
+    # 'Programming Language :: Python :: 2',
+    # 'Programming Language :: Python :: 3',
+    # 'Topic :: Software Development :: Libraries :: Python Modules',
+  ]
+)
